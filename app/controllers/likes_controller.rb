0c77a1be7ae834e5e9c532bacd4e467f74339591
Yuji Hosokai <uugo.hosokai@gmail.com>
2020-08-05 06:47:56 +0900

    LikeControllerの実装　#11

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,21 @@
 class LikesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_product
+
+  def create
+    @like = current_user.likes.create(product_id: params[:product_id])
+    redirect_back(fallback_location: product_path(@product))
+  end
+
+  def destroy
+    @like = Like.find_by(product_id: params[:product_id], user_id: current_user.id)
+    @like.destroy
+    redirect_back(fallback_location: products_path)
+  end
+
+  private
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
 end
