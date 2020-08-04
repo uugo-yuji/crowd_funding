04da4d7f707b54e6881caee3a64b657dd4fa8ab7
Yuji Hosokai <uugo.hosokai@gmail.com>
2020-08-05 06:50:06 +0900

    Userいいねしてるかどうかの判定 #11

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,8 @@ class User < ApplicationRecord
   has_many :products, dependent: :destroy
   has_many :investments
   has_many :liked_products, through: :likes, source: :products
+
+  def already_liked?(product)
+    self.likes.exists?(product_id: product.id)
+  end
 end
