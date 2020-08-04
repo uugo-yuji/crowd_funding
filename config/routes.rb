8a004d58aba5534977d298420a4a8fbba865663a
Yuji Hosokai <uugo.hosokai@gmail.com>
2020-08-04 21:02:32 +0900

    issues10 ProductControllerをadminでの管理用としてnamespaceで追加、scaffoldでの作成,routes設定、ブラウザ上での動作確認済 #10

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   resources :products do
     resources :investments, only: [:index, :new, :create]
   end
+
+  namespace :admin do
+    resources :products
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
