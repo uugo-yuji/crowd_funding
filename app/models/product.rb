2c89d9183976c37b5a47d48f8b25fedf520db89a
Yuji Hosokai <uugo.hosokai@gmail.com>
2020-08-05 07:17:24 +0900

    User,Productモデルアソシエーション記述 #11

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
   belongs_to :user
   has_many :investments, dependent: :destroy
+  has_many :likes, dependent: :destroy
   has_many :liked_users, through: :likes, source: :user
 
   def owner?(user)
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
 
   has_many :products, dependent: :destroy
   has_many :investments
+  has_many :likes, dependent: :destroy
   has_many :liked_products, through: :likes, source: :products
 
   def already_liked?(product)
