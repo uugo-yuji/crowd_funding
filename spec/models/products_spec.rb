113e2d19c78bdd5ab3fa260a9682958501935fc4
Yuji Hosokai <uugo.hosokai@gmail.com>
2020-08-18 00:13:36 +0900

    issue15 コメント箇所修正 https://github.com/uugo-yuji/crowd_funding/pull/15#discussion_r471540817 #15\ https://github.com/uugo-yuji/crowd_funding/pull/15#discussion_r471541699

--- spec/models/products_spec.rb
+++ spec/models/products_spec.rb
@@ -3,15 +3,11 @@ require 'rails_helper'
 RSpec.describe Product, type: :model do
     let(:user) { FactoryBot.create(:user) }
     let(:product) { FactoryBot.create(:product) }
+    let(:my_product) { FactoryBot.create(:product, user: user) }
 
   describe '#owner?(user)' do
     it "tureになること" do
-      product = user.products.build(
-        title: "test",
-        desc: "test1",
-        user_id: user.id
-      )
-      expect(product.owner?(user)).to eq(true)
+      expect(my_product.owner?(user)).to eq(true)
     end
 
     it "falseになること" do
--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -1,18 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  before do
-    @user = build(:user)
-  end
-
-  describe 'バリデーション' do
-    it 'emailの値が設定されればok' do
-      expect(@user).to be_valid
-    end
-
-    it 'emailが空だとNG' do
-      @user.email = ''
-      expect(@user.valid?).to eq(false)
-    end
-  end
 end
