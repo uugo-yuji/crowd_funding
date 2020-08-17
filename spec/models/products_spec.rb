bec72b75e113bfdeed3c9ee091e76bfa9248a9d2
Yuji Hosokai <uugo.hosokai@gmail.com>
2020-08-17 23:52:09 +0900

    issue15 コメント箇所修正 https://github.com/uugo-yuji/crowd_funding/pull/15/commits/ef57a4bb57b70951ed9857d1ef51ae7c7973842f#r470997133 #15

--- spec/models/products_spec.rb
+++ spec/models/products_spec.rb
@@ -1,14 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  before do
-    @user = FactoryBot.create(:user)
-    @product = FactoryBot.create(:product)
-  end
+    let(:user) { FactoryBot.create(:user) }
+    let(:product) { FactoryBot.create(:product) }
 
   describe '#owner?(user)' do
     it "tureになること" do
-      user = @user
       product = user.products.build(
         title: "test",
         desc: "test1",
@@ -18,8 +15,6 @@ RSpec.describe Product, type: :model do
     end
 
     it "falseになること" do
-      user = @user
-      product = @product
       expect(product.owner?(user)).to eq(false)
     end
   end
