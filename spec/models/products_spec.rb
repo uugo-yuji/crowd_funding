b8d38113ae9f11e307787ea38cb05457194ec611
Yuji Hosokai <uugo.hosokai@gmail.com>
2020-08-21 00:18:05 +0900

    issue15 コメント箇所修正 #15

--- spec/models/products_spec.rb
+++ spec/models/products_spec.rb
@@ -2,34 +2,72 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   let(:user) { FactoryBot.create(:user) }
-  let(:owner) { FactoryBot.create(:user) }
   let(:product) { FactoryBot.create(:product) }
-  let(:my_product) { FactoryBot.create(:product, user: user) }
-  let(:like) { FactoryBot.create(:like, user_id: user.id, product_id: my_product.id) }
 
   describe '#owner?(user)' do
-    it "tureになること" do
-      expect(my_product.owner?(user)).to eq(true)
+    context 'ユーザがオーナーの場合' do
+      let(:my_product) { FactoryBot.create(:product, user: user) }
+
+      it "tureになること" do
+        expect(my_product.owner?(user)).to be_truthy
+      end
     end
 
-    it "falseになること" do
-      expect(product.owner?(user)).to eq(false)
+    context 'ユーザがオーナーではない場合' do
+      it "falseになること" do
+        expect(product.owner?(user)).to be_falsey
+      end
     end
+
   end
 
   describe '#liked?(user)' do
-    it "trueになること" do
-      expect(like.product.liked?(user)).to eq(true)
+    context 'プロダクトいいねが存在する場合' do
+      before do
+        FactoryBot.create(:like, user: user, product: product)
+      end
+    
+      it "trueになること" do
+        expect(product.liked?(user)).to be_truthy
+      end
     end
+    
+    context 'プロダクトいいねが存在しない場合' do
+      let(:owner) { FactoryBot.create(:user) }
 
-    it "falseになること" do
-      expect(like.product.liked?(owner)).to eq(false)
+      before do
+        FactoryBot.create(:like, user: user, product: product)
+      end
+
+      it "falseになること" do
+        expect(product.liked?(owner)).to be_falsey
+      end
     end
   end
 
   describe '#get_like_by(user)' do
-    it 'likeテーブルの主キーが返ってくること' do
-      expect(like.product.get_like_by(user).id).to eq(like.id)
+    context '特定のproductとuserに紐付いたlikeのレコードが取れる場合' do
+      before do
+        @like = FactoryBot.create(:like, user: user, product: product)
+      end
+
+      it 'likeが返ってくること' do
+        expect(product.get_like_by(user)).to eq(@like)
+      end
+
+    end
+
+    context '特定のproductとuserに紐付いたlikeのレコードが取れない場合' do
+      let(:my_product) { FactoryBot.create(:product, user: user) }
+
+      before do
+        FactoryBot.create(:like, user: user, product: product)
+      end
+
+      it 'likeのレコードが返ってこないこと' do
+        expect(my_product.get_like_by(user)).to eq(nil)
+      end
+
     end
   end
 
