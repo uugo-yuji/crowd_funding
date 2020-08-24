849a47ae0dd0af45385f22bd99b77056efa082e8
Yuji Hosokai <uugo.hosokai@gmail.com>
2020-08-24 17:37:49 +0900

    Productテーブルにstatusカラムの追加 db:migrate済 #16

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_04_212030) do
+ActiveRecord::Schema.define(version: 2020_08_24_083422) do
 
   create_table "investments", force: :cascade do |t|
     t.integer "user_id"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2020_08_04_212030) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id", null: false
+    t.integer "status"
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
