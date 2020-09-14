16af0e048c8a30f8ee5f922d8b262342424c7bec
Yuji Hosokai <uugo.hosokai@gmail.com>
2020-09-15 02:07:14 +0900

    productが目標金額を満たしている場合の投資画面へのURL直打ち防止の実装 ブラウザ動作確認済 #16

--- app/controllers/investments_controller.rb
+++ app/controllers/investments_controller.rb
@@ -1,5 +1,6 @@
 class InvestmentsController < ApplicationController
   # before_action :set_investment, only: [:show, :edit, :update, :destroy]
+  before_action :incorrect_user, only: [:new]
 
   # GET /investments
   # GET /investments.json
@@ -72,5 +73,12 @@ class InvestmentsController < ApplicationController
     # Only allow a list of trusted parameters through.
     def investment_params
       params.require(:investment).permit(:price)
+    end
+
+    def incorrect_user
+      @product = Product.find(params[:product_id])
+      if @product.complete?
+        redirect_to product_path(@product)
+      end
     end
 end
