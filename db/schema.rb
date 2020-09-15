358c03db727a8d1f5c969af590eb4673e7266da4
Yuji Hosokai <uugo.hosokai@gmail.com>
2020-09-15 10:33:18 +0900

    roleカラムマグレーションの実行 #17

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_25_091415) do
+ActiveRecord::Schema.define(version: 2020_09_15_013048) do
 
   create_table "investments", force: :cascade do |t|
     t.integer "user_id"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 2020_08_25_091415) do
     t.datetime "locked_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "role", default: 0, null: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
