6673fae093f1c0ae52ad74c3fd23a8cf4a057f30
Yuji Hosokai <uugo.hosokai@gmail.com>
2020-09-15 11:35:47 +0900

    administer/investmentsへはadminの権限持つUserしかアクセス出来ない様に実装 #17

--- app/controllers/administer/investments_controller.rb
+++ app/controllers/administer/investments_controller.rb
@@ -1,7 +1,16 @@
 class Administer::InvestmentsController < ApplicationController
+  before_action :admin_user
 
   def index
     @investments = Investment.all
   end
 
+  private
+
+  def admin_user
+    @user = current_user
+    if @user.general?
+      redirect_to products_path
+    end
+  end
 end
