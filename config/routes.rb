4dc3d559c6d49a91da2f9fa1e5e31c571c27c97f
Yuji Hosokai <uugo.hosokai@gmail.com>
2020-09-18 23:00:50 +0900

    administer直下にCategoryのCRUDを作成 migrate前 #18

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
 
   namespace :administer do
     resources :investments, only: [:index]
+    resources :categories
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
