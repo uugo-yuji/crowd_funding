c8b6ce13d385a2454fe29f32c99502e27d8375a4
Yuji Hosokai <uugo.hosokai@gmail.com>
2020-09-21 00:26:29 +0900

    Product作成時にセレクトボックスでCategoryを選択できる様にする ブラウザ動作確認済 #19

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,7 +4,7 @@ class Product < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :liked_users, through: :likes, source: :user
   has_many :product_categories
-  has_many :categories, through: :product_categories
+  has_many :categories, through: :product_categories, dependent: :destroy
 
   enum status: { active: 0, complete: 1 }
 
