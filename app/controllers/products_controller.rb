4ca710ff97b5aedfe11cb77dd2e8426b3fd0c6c6
Yuji Hosokai <uugo.hosokai@gmail.com>
2020-10-05 22:58:45 +0900

    issue26 コメント箇所修正 https://github.com/uugo-yuji/crowd_funding/pull/29#discussion_r499480417 #26

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,7 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
-    @products = Product.all.search(search_params)
+    @products = Product.search(search_params)
   end
 
   # GET /products/1
