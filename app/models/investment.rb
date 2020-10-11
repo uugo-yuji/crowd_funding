a56d79bc36cdba45c9265301b8ee577e1f41b73f
Yuji Hosokai <uugo.hosokai@gmail.com>
2020-10-12 03:50:20 +0900

    issue30 investmentバリデーション 空文字不可を追加 #30

--- app/models/investment.rb
+++ app/models/investment.rb
@@ -2,5 +2,5 @@ class Investment < ApplicationRecord
   belongs_to :user
   belongs_to :product
 
-  validates :price, numericality: { only_integer: true }
+  validates :price, presence: true, numericality: { only_integer: true }
 end
