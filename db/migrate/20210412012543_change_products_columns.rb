9f311b0dd1dd8a02485c52d3da51fc9d7f699ffe
Yuji Hosokai <uugo.hosokai@gmail.com>
2021-04-12 10:32:45 +0900

    productsテーブルのstatusカラムにdefault値を付与 db:migrate済 #40

---
+++ db/migrate/20210412012543_change_products_columns.rb
@@ -0,0 +1,5 @@
+class ChangeProductsColumns < ActiveRecord::Migration[6.0]
+  def change
+    add_column :products, :status, :integer, default: 0 
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_28_093550) do
+ActiveRecord::Schema.define(version: 2021_04_12_012543) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name", null: false
@@ -90,8 +90,8 @@ ActiveRecord::Schema.define(version: 2020_11_28_093550) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
-    t.integer "status"
     t.integer "goal_price"
+    t.integer "status", default: 0
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
